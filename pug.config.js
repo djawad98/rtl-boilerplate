1a1272889c2814863e1899deab90ef09aadc3f94
javad <ghasedjavad@gmail.com>
2021-08-23 12:02:54 +0430

    Create boilerplate for ltr to rtl project.
    Includes sass, pug, vscode tasks and project specific settings.

---
+++ pug.config.js
@@ -0,0 +1,10 @@
+{
+    vars: {
+        title: "گروه آراز - کاغذ دیواری، کفپوش، پارکت، دیوارپوش",
+        projects: [],
+        products: [],
+        services: [],
+        clients: [],
+        certificates: []
+    }
+}

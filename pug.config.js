1c76a858366a5b6d86880153f8438effd2e5c9b9
djawad98 <49300902+djawad98@users.noreply.github.com>
2021-08-24 16:36:29 +0430

    Changes pug.config.js to .json. For the sake of vscode unreasonable errors.

--- pug.config.js
+++ pug.config.js
@@ -1,10 +1,16 @@
 {
-    vars: {
-        title: "گروه آراز - کاغذ دیواری، کفپوش، پارکت، دیوارپوش",
-        projects: [],
-        products: [],
-        services: [],
-        clients: [],
-        certificates: []
+    "vars": {
+        "title": "پیام سرما - برج خنک کننده | طراحی، اجرا و مشاوره رایگان",
+        "logo": "/Content/img/logo.png",
+        "logo2": "/Content/img/logo-2.png",
+        "address": "مشهد، فلسطین 12، پلاک 89، واحد 3",
+        "email": "info@payamsarma.com",
+        "phone1": "051-3245678",
+        "phone2": "+98-915-123-4567",
+        "projects": [],
+        "products": [],
+        "services": [],
+        "clients": [],
+        "certificates": []
     }
 }

c0670d1e1d7e2affcdd10fbfaa272ea320acf6a3
djawad <ghasedjavad@gmail.com>
2021-09-15 11:33:10 +0430

    Optimize Build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
-cd ./build
+build_folder="./dist"
+
+# Re-Build the project
+./node_modules/.bin/cross-env NODE_ENV=production npm run pugjs
+./node_modules/.bin/cross-env NODE_ENV=production npm run postcss
+
+mkdir "$build_folder"
+cd "$build_folder"
+
 mkdir Content
+
 cd ../
-cp -rf ./public/* ./build/Content
-rar a ./build.zip ./build/*
+
+
+cp -rf ./build/* "$build_folder"
+cp -rf ./public/* "$build_folder"/Content
+
+# Delete useless dev files
+rm "$build_folder"/*.map
+
+# Make a zip file recursively and delete the source files
+rar a -r -dr "$build_folder".zip "$build_folder"
